04ac03c2d66b1b8b0cbfbebd2750f09fc8da380c
Administrator <weixingwanglalaxiu@163.com>
2016-12-02 17:05:08 +0800

    首页的完善,tab的完善(徐)

--- app/src/main/java/com/weixingwang/threepomelo/frament/HomeFragment.java
+++ app/src/main/java/com/weixingwang/threepomelo/frament/HomeFragment.java
@@ -3,17 +3,18 @@ package com.weixingwang.threepomelo.frament;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 import com.weixingwang.threepomelo.R;
-import com.weixingwang.threepomelo.adapter.BasePagerAdapter;
+import com.weixingwang.threepomelo.adapter.BaseRecyleAdapter;
+import com.weixingwang.threepomelo.adapter.HomeFragmentPagerAdapter;
+import com.weixingwang.threepomelo.adapter.HomeFragmentRecyleAdapter;
+import com.weixingwang.threepomelo.view.MyScrollView;
 
 import java.util.ArrayList;
 
@@ -28,6 +29,7 @@ public class HomeFragment extends BaseFragment {
     private int[] color={Color.BLUE,Color.BLACK,Color.RED,Color.WHITE,Color.YELLOW};
     private LinearLayout linIndc;
     private int pos;
+    private ArrayList<String> reList=new ArrayList<>();
 
     private Handler handler=new Handler(){
         @Override
@@ -42,6 +44,8 @@ public class HomeFragment extends BaseFragment {
             handler.sendEmptyMessageDelayed(1,2000);
         }
     };
+    private MyScrollView sw;
+
     @Override
     protected int getLayoutId() {
         return R.layout.home_frament_layout;
@@ -49,11 +53,16 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
+        SwipeRefreshLayout swrf= (SwipeRefreshLayout) view.findViewById(R.id.home_fragment_swf);
+        sw = (MyScrollView) view.findViewById(R.id.home_fragment_msw);
         vp = (ViewPager) view.findViewById(R.id.home_frament_vp);
         recl = (RecyclerView) view.findViewById(R.id.home_fragment_recl);
         linIndc = (LinearLayout) view.findViewById(R.id.lin_home_fragment_indector);
+        setSwColor(swrf);
         isShowArea(true);
         isShowSearch(true);
+        isReflash(sw);
+
     }
 
     @Override
@@ -67,7 +76,7 @@ public class HomeFragment extends BaseFragment {
 //            imageView.setLayoutParams(pm);
             imageList.add(imageView);
         }
-        vp.setAdapter(new BasePagerAdapter(imageList));
+        vp.setAdapter(new HomeFragmentPagerAdapter(imageList));
         for (int i = 0; i < imageList.size(); i++) {
             ImageView imageView = new ImageView(getActivity());
             imageView.setImageResource(R.drawable.indector_selector);
@@ -78,7 +87,17 @@ public class HomeFragment extends BaseFragment {
             linIndc.addView(imageView);
         }
         linIndc.getChildAt(0).setSelected(true);
+        initRecleData();
         handler.sendEmptyMessageDelayed(1,2000);
+
+
+    }
+
+    private void initRecleData() {
+        for (int i = 0; i < 20; i++) {
+            reList.add("数据"+i);
+        }
+        recl.setAdapter(new HomeFragmentRecyleAdapter(getActivity(),recl,reList,R.layout.home_frament_recle_item,1));
     }
 
     @Override
