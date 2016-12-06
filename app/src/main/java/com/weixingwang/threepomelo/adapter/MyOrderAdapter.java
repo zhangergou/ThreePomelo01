c79363ad3f7eec4348212d61ae0876e6043b1256
Administrator <939085247@qq.com>
2016-12-06 11:58:23 +0800

    修改MyOrderFragment页面（孟）

--- app/src/main/java/com/weixingwang/threepomelo/adapter/MyOrderAdapter.java
+++ app/src/main/java/com/weixingwang/threepomelo/adapter/MyOrderAdapter.java
@@ -5,20 +5,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.weixingwang.threepomelo.R;
-import com.weixingwang.threepomelo.bean.MyOrderBean;
-
 import java.util.ArrayList;
-
-/**
- * Created by Administrator on 2016/12/2.
- */
 public class MyOrderAdapter extends RecyclerView.Adapter<MyOrderAdapter.MyHolder>  {
    private Context mcontent;
     private ArrayList<String> list;
     private LayoutInflater inflater;
+    private LinearLayout linearLayout;
+   private boolean visibility_Flag=false;
+    private LinearLayout load_more;
     @Override
     public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View inflate=inflater.inflate(R.layout.item_my_order,null);
@@ -31,21 +30,36 @@ public class MyOrderAdapter extends RecyclerView.Adapter<MyOrderAdapter.MyHolder
       this.inflater=LayoutInflater.from(context);
   }
     @Override
-    public void onBindViewHolder(MyHolder holder, int position) {
+    public void onBindViewHolder(MyHolder holder, final int position) {
       holder.tv.setText(list.get(position));
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(visibility_Flag){
+                    load_more.setVisibility(View.INVISIBLE);
+                    visibility_Flag = false;
+                } else {
+                    load_more.setVisibility(View.VISIBLE);
+                    visibility_Flag =true;
+                }
+                Toast.makeText(mcontent, position+"position=", Toast.LENGTH_SHORT).show();
+                notifyDataSetChanged();
+            }
+        });
     }
-
     @Override
     public int getItemCount() {
         return list.size();
     }
-
     class MyHolder extends RecyclerView.ViewHolder {
-
        private TextView tv;
+        private LinearLayout linearLayout;
         public MyHolder(View itemView) {
             super(itemView);
             tv= (TextView) itemView.findViewById(R.id.tv);
+            linearLayout= (LinearLayout) itemView.findViewById(R.id.linearLayout);
+            load_more= (LinearLayout) itemView.findViewById(R.id.load_more);
+
         }
     }
 }
--- app/src/main/java/com/weixingwang/threepomelo/frament/MyOrderFragment.java
+++ app/src/main/java/com/weixingwang/threepomelo/frament/MyOrderFragment.java
@@ -2,17 +2,15 @@ package com.weixingwang.threepomelo.frament;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.weixingwang.threepomelo.R;
 import com.weixingwang.threepomelo.adapter.MyOrderAdapter;
 import com.weixingwang.threepomelo.utils.OkHttpUtils;
 
 import java.util.ArrayList;
-
-/**
- * Created by Administrator on 2016/11/29 0029.
- */
 public class MyOrderFragment extends BaseFragment {
     private RecyclerView rcv;
     private ArrayList<String> list = new ArrayList<>();
@@ -21,28 +19,24 @@ public class MyOrderFragment extends BaseFragment {
     protected int getLayoutId() {
         return R.layout.my_order_frament_layout;
     }
-
     @Override
     protected void initView(View view) {
-       // setTitle("我的订单");
-       rcv = (RecyclerView)view.findViewById(R.id.recyclerview);
+        setTitle("我的订单");
+        rcv = (RecyclerView)view.findViewById(R.id.recyclerview);
         //Recyclerview线性排列
         rcv.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false));//从零开始往下
         setTitle("我的订单");
         adapter=new MyOrderAdapter(getActivity(),list);
         rcv.setAdapter(adapter);
+       // adapter.notifyDataSetChanged();
     }
-
     @Override
     protected void initData() {
         for(int i=0;i<20;i++){
             list.add("item"+i);
         }
     }
-
     @Override
     protected void initLisener() {
     }
-
-
 }
